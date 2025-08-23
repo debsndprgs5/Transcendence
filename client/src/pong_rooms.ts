93913300a72a2323d6b793a37855d5b8d76304c9
hyyven <afont@student.42nice.fr>
2025-08-23 20:55:46 +0200

    ah oui j'avais oublie ca

--- client/src/pong_rooms.ts
+++ client/src/pong_rooms.ts
@@ -54,7 +54,7 @@ export interface CreateGameFormData {
 
 export const createGameFormData: CreateGameFormData = {
   roomName:     null,
-  ballSpeed:    500, // #achanger
+  ballSpeed:    50,
   paddleSpeed:  50,
   mode:         'duo',
   winCondition: 'time',
